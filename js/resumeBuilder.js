a487409daeee3705c11ac1fc19db48bbff024f3b
Nursultan Mussabayev <nursalga@gmail.com>
2016-06-29 17:21:54 +0600

    29.06.2016 workplace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,55 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+    'name': "Nursultan",
+    'role': "Web Developer",
+    'contact info': {
+        "mobile":  '87455663121',
+        "email": "nursalga@gmail.com",
+        "location": 'Shymkent'
+    },
+    'picture': "../images/foto.jpg",
+    "skills" :["good learning","work hard"]
+
+};
+var education = {
+    "school":{
+        "school's Number": 9,
+        "location": "Shymkent",
+        "rating": "1st school in a town"
+    },
+    "university": 'Almaty radio and telecom university',
+    "location": "Almaty",
+    "rating": "3-rd university in Kazahstan",
+    "specialization" :"Telecommunications and radio"
+    "chair": "Radio"
+};
+
+var work = {
+    1:{
+        "employer": "Astanatelecom",
+        "location": "Astana",
+        "role": "engineer of Help Desk",
+        "salary" : 90000
+    },
+    2:{
+        "employer": "IT'S",
+        "location": "Shymkent",
+        "role" : "sales manager",
+        "salary": 20000
+    },
+    3:{
+       "employer" : "Kaspi Bank",
+        "location" : "Shymkent",
+        "role": "engineer of IT department",
+        "salary": 84000
+    }
+};
+var projects = {
+    1:{
+        "type of project" : "site",
+        "project's Name": "Greengo.kz",
+        "dates": 2016
+    }
+};
