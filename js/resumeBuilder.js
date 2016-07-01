f78838e3d9310310549a4b2f59fd6ba6f72377f0
Nursultan Mussabayev <nursalga@gmail.com>
2016-07-01 19:48:36 +0600

    01.07.2016

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -22,34 +22,51 @@ var education = {
     "university": 'Almaty radio and telecom university',
     "location": "Almaty",
     "rating": "3-rd university in Kazahstan",
-    "specialization" :"Telecommunications and radio"
+    "specialization" :"Telecommunications and radio",
     "chair": "Radio"
 };
 
 var work = {
-    1:{
-        "employer": "Astanatelecom",
-        "location": "Astana",
-        "role": "engineer of Help Desk",
-        "salary" : 90000
+    "jobs": [
+        {   "employer": "Astanatelecom",
+            "title": "engineer of Help Desk",
+            "location": "Astana",
+            "salary" : 90000
     },
-    2:{
+    {
         "employer": "IT'S",
+        "title" : "sales manager",
         "location": "Shymkent",
-        "role" : "sales manager",
         "salary": 20000
     },
-    3:{
+    {
        "employer" : "Kaspi Bank",
         "location" : "Shymkent",
-        "role": "engineer of IT department",
+        "title": "engineer of IT department",
         "salary": 84000
     }
-};
+]
+}
 var projects = {
     1:{
         "type of project" : "site",
         "project's Name": "Greengo.kz",
-        "dates": 2016
+        "dates": 2016.06
     }
 };
+if(bio.skills.length> 0){
+
+    $('#header').append(HTMLskillsStart);
+    var formSkills= HTMLskills.replace("%data%", bio.skills[0]);
+    $('#skills').append(formSkills);
+    formSkills= HTMLskills.replace("%data%",bio.skills[1]);
+    $('#skills').append(formSkills);
+}
+for(job in work){
+    $("#workExperience").append(HTMLworkStart);
+    var formattedJobEmployer=HTMLworkEmployer.replace("%data%",work.jobs[job].title);
+    var formattedJobTitle=HTMLworkTitle.replace("%data%",work.jobs[job].title);
+    var formattedJobTitle=formattedJobEmployer+formattedJobTitle;
+
+    $(".work-entry:last").append(formattedJobTitle);
+}
