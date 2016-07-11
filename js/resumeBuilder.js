0b0b682503f64f7a9db245eda8b4cbb2a551146c
Nursultan Mussabayev <nursalga@gmail.com>
2016-07-11 16:40:21 +0600

    11.07.2016

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,14 +2,14 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 var bio = {
-    'name': "Nursultan",
+    'name': "Nursultan Mussabayev",
     'role': "Web Developer",
     'contact info': {
         "mobile":  '87455663121',
         "email": "nursalga@gmail.com",
         "location": 'Shymkent'
     },
-    'picture': "../images/foto.jpg",
+    'picture': "images/foto.jpg",
     "skills" :["good learning","work hard"]
 
 };
@@ -54,12 +54,23 @@ var work = {
 ]
 }
 var projects = {
-    1:{
-        "type of project" : "site",
-        "project's Name": "Greengo.kz",
-        "dates": 2016.06
-    }
+    projects:[{
+        "title" : "Greengo.kz",
+        "dates": "2016.06",
+        "description" : "Site of the WebDeveloper company, called so!!! They make a web-advertisement.",
+        "picture": ["images/images.png", "images/images1.png"]
+    }]
 };
+bio.display = function(){
+    var formattedName= HTMLheaderName.replace("%data%", bio.name);
+    $("#header").append(formattedName);
+    var formattedRole=HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").append(formattedRole);
+    var formattedPic=HTMLbioPic.replace("%data%", bio.picture);
+    $("#header").append(formattedPic);
+}
+bio.display();
+
 if(bio.skills.length> 0){
 
     $('#header').append(HTMLskillsStart);
@@ -85,7 +96,27 @@ function displayWork() {
 }
 displayWork();
 
-$("#main").append(internationalizeButton);
+$("#header").append(internationalizeButton);
+function inName(name){
+    var splitedName = name.split(" ");
+    return splitedName[0].slice(0,1).toUpperCase()+splitedName[0].slice(1).toLowerCase()+" "+splitedName[1].toUpperCase();
+}
+ projects.display = function(){
+    for (proj in projects.projects){
+        $("#projects").append(HTMLprojectStart);
+        var formattedProjTitle = HTMLprojectTitle.replace("%data%", projects.projects[proj].title);
+        $(".project-entry:last").append(formattedProjTitle);
+        var formattedProjDates= HTMLprojectDates.replace("%data%", projects.projects[proj].dates);
+        $(".project-entry:last").append(formattedProjDates);
+        var formattedProjDescription= HTMLprojectDescription.replace("%data%", projects.projects[proj].description);
+        $(".project-entry:last").append(formattedProjDescription);
+        for (pic in projects.projects[proj].picture){
+            var formattedProjPicture = HTMLprojectImage.replace("%data%", projects.projects[proj].picture[pic]);
+            $(".project-entry:last").append(formattedProjPicture);
+        }
+    }
+}
+projects.display();
 
 
 
